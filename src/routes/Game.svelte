3850374e1e187536bc7fe0f2002a8e5d88670ce1
romuloban <romulo88@hotmail.com>
2023-06-23 18:15:55 +0200

    initial commit

---
+++ src/routes/Game.svelte
@@ -0,0 +1,24 @@
+<div class="game">
+    <div class="info">
+
+    </div>
+    <div class="grid-container">
+
+    </div>
+    <div class="info">
+
+    </div>
+</div>
+
+<style>
+    .game {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .info {
+        width: 80vmin;
+    }
+</style>
